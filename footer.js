0b79ae2c644b79ab53e7ef12c96a204e8b11c1f5
leogermani <leogermani@gmail.com>
2020-03-21 16:47:02 -0300

    esconde campos de local do form de eventos

---
+++ footer.js
@@ -0,0 +1,11 @@
+(function($) {
+
+	$(document).ready(function() {
+
+		$('#no-location').click();
+		$('h3.event-form-where').hide();
+		$('div.event-form-where').hide();
+
+	});
+
+})( jQuery );
--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function cff_enqueue_styles() {
 	$parent_style = 'tainacan-interface';
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_script( $parent_style, get_stylesheet_directory_uri() . '/footer.js', ['jquery'], '1.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'cff_enqueue_styles', 99 );

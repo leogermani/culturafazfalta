597413c417d825f35d8502907214b161aefb4438
brmagrini@gmail.com <brmagrini@gmail.com>
2020-03-22 00:38:12 -0300

    melhora a funcao para o scroll do mapa mais uma vez

--- footer.js
+++ footer.js
@@ -7,9 +7,15 @@
 		$('div.event-form-where').hide();
 		$('#mapa_iframe')
 		.click(function(){
-				$(this).find('iframe').toggleClass('clicked')})
+			$(this).find('iframe').toggleClass('clicked')
+		})
 		.mouseleave(function(){
-				$(this).find('iframe').removeClass('clicked')});
+			$(this).find('iframe').removeClass('clicked')
+		});
+		$('#mapa_iframe iframe')
+			.click(function(){
+				$(this).toggleClass('clicked')
+			})
 	});
 
 })( jQuery );

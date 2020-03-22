4fd76b61094ae23443cf8445da3e2c7bb06dfb61
brmagrini@gmail.com <brmagrini@gmail.com>
2020-03-22 00:09:19 -0300

    adiciona js para scroll do mapa

--- footer.js
+++ footer.js
@@ -5,7 +5,11 @@
 		$('#no-location').click();
 		$('h3.event-form-where').hide();
 		$('div.event-form-where').hide();
-
+		$('#mapa_iframe')
+		.click(function(){
+				$(this).find('iframe').addClass('clicked')})
+		.mouseleave(function(){
+				$(this).find('iframe').removeClass('clicked')});
 	});
 
 })( jQuery );

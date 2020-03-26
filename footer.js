440af75ff4693c46456d9b5e72191d9b4f44d1c9
leogermani <leogermani@gmail.com>
2020-03-26 09:04:40 -0300

    volta com local para eventos

--- footer.js
+++ footer.js
@@ -2,9 +2,12 @@
 
 	$(document).ready(function() {
 
-		$('#no-location').click();
-		$('h3.event-form-where').hide();
-		$('div.event-form-where').hide();
+		// Esconde a parte de definir local para eventos no formulário
+		// $('#no-location').click();
+		// $('h3.event-form-where').hide();
+		// $('div.event-form-where').hide();
+
+
 		$('#mapa_iframe')
 		.click(function(){
 			$(this).find('iframe').toggleClass('clicked')
